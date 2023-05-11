038841eba67d36ff985853a8cabea0483b4c69e1
ISSolucoes <117482741+ISSolucoes@users.noreply.github.com>
2023-05-11 10:59:59 -0300

    Icones e splashScreen
    Resultado final da aplicação, já destinada a loja de aplicativos.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <qnativeinterface.h>
 
 int main(int argc, char *argv[])
 {
+    QNativeInterface::QAndroidApplication::hideSplashScreen(3000);
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
